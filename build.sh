cf9f4708f6ce7cda0405cc4496fcf4528cc630be
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-02-23 16:48:25 +0100

    change build script to new guests

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-cargo build -p guest --target wasm32-unknown-unknown --release
+cargo build -p pmod-oled-driver --target wasm32-unknown-unknown --release
+cargo build -p pacman --target wasm32-unknown-unknown --release
+wasm-tools component new target/wasm32-unknown-unknown/release/pacman.wasm -o target/wasm32-unknown-unknown/release/pacman.component.wasm
+wasm-tools component new target/wasm32-unknown-unknown/release/pmod_oled_driver.wasm -o target/wasm32-unknown-unknown/release/pmod_oled_driver.component.wasm
+wac plug target/wasm32-unknown-unknown/release/pacman.component.wasm --plug target/wasm32-unknown-unknown/release/pmod_oled_driver.component.wasm -o pacman.wasm
 cargo run -p compiler -- unknown
 cd pico2-quick
 cargo run --release
